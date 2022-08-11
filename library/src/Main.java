68d5fa9afa68d7f4cc7d4bc0f619eca85509c9df
Caleb P. Nwokocha <nwo20001@byui.edu>
2022-08-10 21:18:45 -0500

    Added example

--- library/src/Main.java
+++ library/src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    static void main (String[] args) {
+import java.util.Arrays;
 
+public class Main {
+    public static void main (String[] args) {
+        double[][] matrix1 = {
+                {1, 2},
+                {3, 4}
+        };
+
+        double[][] matrix2 = {
+                {1, 2, 5},
+                {3, 4, 6},
+                {7, 8, 9}
+        };
+
+        Algebra algebra = new Algebra();
+
+        double[][] matrixDotProduct = algebra.multiply(matrix1, matrix2);
+
+        System.out.println("The dot-product of " + Arrays.deepToString(matrix1) + " and " +
+                Arrays.deepToString(matrix2) + " is " + Arrays.deepToString(matrixDotProduct) + ".");
     }
 }
